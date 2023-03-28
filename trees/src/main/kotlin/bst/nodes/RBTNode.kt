095a1f3b9528d82b56af5d6b79d146a4d5652406
ancavar <danaj2004@yandex.ru>
2023-03-29 01:19:13 +0300

    feat: implement insert method

--- trees/src/main/kotlin/bst/nodes/RBTNode.kt
+++ trees/src/main/kotlin/bst/nodes/RBTNode.kt
@@ -1,28 +1,13 @@
 package bst.nodes
 
-enum class Color{
-    BLACK,
-    RED
-}
 
 // хз насчет data class, мб че-то заходим в самой ноде делать
 class RBTNode<K: Comparable<K>, V> (
     override val key: K,
     override var value: V,
+    var red: Boolean = true,
 ): TreeNode<K, V, RBTNode<K, V>> {
     override var left: RBTNode<K, V>? = null
     override var right: RBTNode<K, V>? = null
-    var color = Color.BLACK
-
-    internal fun isBlack() {
-        TODO()
-    }
-    internal fun isRed() {
-        TODO()
-    }
-    internal fun flipColors() {
-        TODO()
-    }
-
 
 }
