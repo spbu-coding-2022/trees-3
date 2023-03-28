1c58d50215ca905090bbfc7efd0544e01a0df96e
ancavar <danaj2004@yandex.ru>
2023-03-29 00:58:24 +0300

    feat: implement rotations in BalancingTree

--- trees/src/main/kotlin/bst/BalancingTree.kt
+++ trees/src/main/kotlin/bst/BalancingTree.kt
@@ -1,10 +1,18 @@
 package bst
 
+import bst.nodes.TreeNode
+
 abstract class BalancingTree<K: Comparable<K>, V>: BinarySearchTree<K, V>() {
-    protected fun rotationRight() {
-        TODO()
+    protected fun <SpecNode : TreeNode<K, V, SpecNode>> rotateLeft(node: SpecNode): SpecNode {
+        val right = node.right
+        node.right = right?.left
+        right?.left = node
+        return right!!
     }
-    protected fun rotationLeft() {
-        TODO()
+    protected fun <SpecNode : TreeNode<K, V, SpecNode>> rotateRight(node: SpecNode): SpecNode {
+        val left = node.left
+        node.left = left?.right
+        left?.right = node
+        return left!!
     }
 }
