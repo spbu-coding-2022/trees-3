04961ce926e8d622b50529d64c4d8125b6416a23
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2023-03-28 21:04:58 +0300

    add insert method and private recursiveInsert method to BinarySearchTree class

--- trees/src/main/kotlin/bst/BinarySearchTree.kt
+++ trees/src/main/kotlin/bst/BinarySearchTree.kt
@@ -5,11 +5,38 @@ import bst.nodes.BSTNode
 class BinarySearchTree<K: Comparable<K>, V>: Tree<K, V> {
     var rootNode: BSTNode<K, V>? = null
     override fun insert(key: K, value: V) {
+        val newNode = BSTNode(key, value)
+        if (this.rootNode === null){
+            this.rootNode = newNode
+            return
+        }
+        else{
+            insertRecursive(this.rootNode!!, newNode)
+        }
         //add element
         //make element as a root, if root is null
         //if root is not null it adds element to left or right branch
         //if left or right is not null - it checks it, and adds it if right or left is null
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+    }
+
+    private fun insertRecursive(currentNode: BSTNode<K, V>, newNode: BSTNode<K, V>){
+        if(newNode.key < currentNode.key){
+            if(currentNode.left?.key === null){
+                currentNode.left = newNode
+            }
+            else{
+                insertRecursive(currentNode.left!!, newNode)
+            }
+        }
+        if(newNode.key > currentNode.key){
+            if(currentNode.right?.key === null){
+                currentNode.right = newNode
+            }
+            else{
+                insertRecursive(currentNode.right!!, newNode)
+            }
+        }
     }
 
     override fun remove(key: K, value: V) {
@@ -35,8 +62,12 @@ class BinarySearchTree<K: Comparable<K>, V>: Tree<K, V> {
 
 fun main(){
     val test_tree = BinarySearchTree<Int, String>()
-    test_tree.rootNode = BSTNode(123, "erefe")
-    val test_1 = mutableListOf<Int>(12341, 324)
-    test_tree.rootNode!!.left = BSTNode(123, "world_0")
-    test_tree.rootNode!!.right = BSTNode(123, "world_1")
+    test_tree.insert(234, "afaraf")
+    test_tree.insert(235, "afaraf")
+    test_tree.insert(232, "afaraf")
+    println("afadf")
+//    test_tree.rootNode = BSTNode(123, "erefe")
+//    val test_1 = mutableListOf<Int>(12341, 324)
+//    test_tree.rootNode!!.left = BSTNode(123, "world_0")
+//    test_tree.rootNode!!.right = BSTNode(123, "world_1")
 }
