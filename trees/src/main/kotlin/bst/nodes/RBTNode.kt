bec99006eed479a07b5cacfb8a1e73008f38234e
ancavar <danaj2004@yandex.ru>
2023-03-29 20:32:45 +0300

    refactor: minor refactoring

--- trees/src/main/kotlin/bst/nodes/RBTNode.kt
+++ trees/src/main/kotlin/bst/nodes/RBTNode.kt
@@ -6,6 +6,6 @@ class RBTNode<K : Comparable<K>, V> (
     override var value: V,
     var red: Boolean = true
 ) : TreeNode<K, V, RBTNode<K, V>> {
-    override var left : RBTNode<K, V>? = null
+    override var left: RBTNode<K, V>? = null
     override var right: RBTNode<K, V>? = null
 }
