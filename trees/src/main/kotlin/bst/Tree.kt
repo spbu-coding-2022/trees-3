5314ceb665e772df1a0181b34f7d2741c023a75a
ancavar <danaj2004@yandex.ru>
2023-03-29 21:48:52 +0300

    fix: remove value parameter from Tree interface

--- trees/src/main/kotlin/bst/Tree.kt
+++ trees/src/main/kotlin/bst/Tree.kt
@@ -2,7 +2,7 @@ package bst
 
 interface Tree<K : Comparable<K>, V> {
     fun insert(key: K, value: V)
-    fun remove(key: K, value: V)
+    fun remove(key: K)
     fun find(key: K): Boolean
     fun clear()
 }
