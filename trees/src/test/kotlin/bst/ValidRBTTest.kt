e201dc33a2e7dcd08c33f4b2d6e900262185da7b
ancavar <danaj2004@yandex.ru>
2023-04-03 23:19:49 +0300

    fix: change tests

--- trees/src/test/kotlin/bst/ValidRBTTest.kt
+++ trees/src/test/kotlin/bst/ValidRBTTest.kt
@@ -8,12 +8,13 @@ class ValidRBTTest {
     @Test
     fun testInsert() {
         val tree = RedBlackTree<Int, String>()
-        tree.insert(1, "One")
-        tree.insert(2, "Two")
-        tree.insert(3, "Three")
+        tree.insert(19, "S")
+        tree.insert(5, "E")
+        tree.insert(1, "A")
+        tree.insert(18, "R")
+        assertEquals(true, tree.find(5))
         assertEquals(true, tree.find(1))
-        assertEquals(true, tree.find(2))
-        assertEquals(true, tree.find(3))
+        assertEquals(false, tree.find(10))
     }
 
     @Test
