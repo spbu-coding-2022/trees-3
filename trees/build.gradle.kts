40c6bf02cd7b913a6d92e654cf49b0c7f35ed421
ancavar <danaj2004@yandex.ru>
2023-04-15 17:09:12 +0300

    ci: implement JaCoCo

--- trees/build.gradle.kts
+++ trees/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("jacoco")
 }
 
 repositories {
@@ -16,7 +17,19 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
 
-tasks.named<Test>("test") {
+tasks.test {
     // Use JUnit Platform for unit tests.
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("reports/jacoco"))
+        csv.required.set(true)
+        csv.outputLocation.set(layout.buildDirectory.file("jacoco/report.csv"))
+    }
 }
