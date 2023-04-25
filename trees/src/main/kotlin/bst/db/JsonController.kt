de4844e7c4f631edfcdd7d1ec5cfbb3bdee84c1a
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2023-04-25 21:00:13 +0300

    fix: Rework tree deserialization logic

--- trees/src/main/kotlin/bst/db/JsonController.kt
+++ trees/src/main/kotlin/bst/db/JsonController.kt
@@ -1,15 +1,13 @@
 package bst.db
 
 import bst.AVLTree
-import bst.BSTree
 import com.google.gson.Gson
 import java.io.FileReader
 import java.io.FileWriter
 
 class JsonController{
-    fun saveTreeToJson(tree: AVLTree<*, *>){
+    fun saveTreeToJson(tree: AVLTree<Int, String>){
         val gson = Gson()
-//        val writer: FileWriter
         try {
             val writer = FileWriter("${tree.treeName}.json")
             gson.toJson(tree, writer)
@@ -18,11 +16,11 @@ class JsonController{
             println("Write error")
         }
     }
-    fun readFromJson(treeName: String): BSTree<*, *>? {
+    fun readFromJson(treeName: String): AVLTree<Int, String>? {
         val gson = Gson()
         return try{
             val reader = FileReader("$treeName.json")
-            val tree = gson.fromJson(reader, BSTree::class.java)
+            val tree = gson.fromJson(reader, AVLTree<Int, String>()::class.java)
             reader.close()
             tree
         }catch (e: Exception){
